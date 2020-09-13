49fdbaeb0363239d96eab925cfe709101af4398e
iceland101113 <iceland101113@gmail.com>
2020-09-13 23:39:58 +0800

    Add leaflet map

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -23,6 +23,40 @@
         <p>Humidity: {{humidity}}%</p>
       </div>
     </section>
+    <br>
+    <!-- 初始化地圖設定 -->
+    <l-map
+      ref="myMap"
+      :zoom="zoom"
+      :center="center"
+      :options="options"
+      style="height: 80vh;">
+      <!-- 載入圖資 -->
+      <l-tile-layer :url="url" :attribution="attribution" />
+      
+      <!-- 所在位置 -->
+      <l-marker ref="location" :lat-lng="center">
+        <l-popup>
+          {{city}}
+        </l-popup>
+      </l-marker>
+      <!-- 創建標記點 -->
+      <!-- <l-marker :lat-lng="item.local" v-for="item in data" :key="item.id"> -->
+        <!-- 標記點樣式判斷 -->
+<!--         <l-icon
+          :icon-url="item.name === '夢時代購物中心'?icon.type.gold:icon.type.black"
+          :shadow-url="icon.shadowUrl"
+          :icon-size="icon.iconSize"
+          :icon-anchor="icon.iconAnchor"
+          :popup-anchor="icon.popupAnchor"
+          :shadow-size="icon.shadowSize"
+        /> -->
+        <!-- 彈出視窗 -->
+        <!-- <l-popup> -->
+          <!-- {{ item.name }} -->
+        <!-- </l-popup> -->
+      <!-- </l-marker> -->
+    </l-map>
   </div>
 </template>
 
@@ -42,6 +76,37 @@
           humidity: null,
           icon: '',
           api_key: 'd21dc464301d51371f88a65e68ccc7f1',
+          data: [
+            { id: 1, name: "夢時代購物中心", local: [22.595153, 120.306923] },
+            { id: 2, name: "漢神百貨", local: [22.61942, 120.296386] },
+            { id: 3, name: "漢神巨蛋", local: [22.669603, 120.302288] },
+            { id: 4, name: "大統百貨", local: [22.630748, 120.318033] }
+          ],
+          
+          zoom: 11,
+          center: [23.781389, 120.958699],
+          url: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+          attribution: `© <a href="http://osm.org/copyright">OpenStreetMap</a> contributors`,
+          options: {
+            zoomControl: true
+          },
+          icon: {
+            type: {
+              black:
+                "https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-black.png",
+              gold:
+                "https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-gold.png"
+            },
+            shadowUrl:
+              "https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png",
+            iconSize: [25, 41],
+            iconAnchor: [12, 41],
+            popupAnchor: [1, -34],
+            popupContent: {
+              maxWidth: 1000
+            },
+            shadowSize: [41, 41]
+          }
         };
     },
     methods: {
@@ -49,6 +114,7 @@
         this.$http
           .get(`/weather?q=${this.query}&units=metric&&appid=${this.api_key}`)
           .then(response => {
+            console.log(response)
             this.city = response.data.name;
             this.country = response.data.sys.country;
             this.weatherDescription = response.data.weather[0].description;
@@ -59,6 +125,9 @@
             this.icon = `http://openweathermap.org/img/w/${
               response.data.weather[0].icon
             }.png`;
+            this.center = [response.data.coord.lat, response.data.coord.lon];
+            // this.zoom = 11
+            this.$refs.location.mapObject.openPopup();
             this.error = false;
           })
           .catch(() => {
@@ -66,6 +135,29 @@
             this.city = '';
           });
       },
+    },
+    mounted() {
+      console.log('haha')
+        navigator.geolocation.getCurrentPosition(position => { 
+          const p = position.coords;
+          console.log(p)
+          // 將中心點設為目前的位置
+          this.center = [p.latitude, p.longitude];
+          // 將目前的位置的標記點彈跳視窗打開
+          // this.$refs.location.mapObject.openPopup();
+        });
+      // 等地圖創建後執行
+      this.$nextTick(() => {
+        // 獲得目前位置
+        navigator.geolocation.getCurrentPosition(position => { 
+          const p = position.coords;
+          console.log(p)
+          // 將中心點設為目前的位置
+          this.center = [p.latitude, p.longitude];
+          // 將目前的位置的標記點彈跳視窗打開
+          // this.$refs.location.mapObject.openPopup();
+        });
+      });
     }
   }
 </script>
@@ -85,25 +177,26 @@
     font-family: Arial, Helvetica, sans-serif;
     font-size: 16px;
     padding: 10px;
-    background: rgb(212, 228, 239);
+    background: rgb(228, 233, 237);
+    /*background: lightgrey;*/
     background: -moz-radial-gradient(
       center,
       ellipse cover,
-      rgba(212, 228, 239, 1) 0%,
-      rgba(134, 174, 204, 1) 100%
+      rgba(228, 233, 237, 1) 0%,
+      rgba(206, 214, 219, 1) 100%
     );
     background: -webkit-radial-gradient(
       center,
       ellipse cover,
-      rgba(212, 228, 239, 1) 0%,
-      rgba(134, 174, 204, 1) 100%
+      rgba(228, 233, 237, 1) 0%,
+      rgba(206, 214, 219, 1) 100%
     );
     background: radial-gradient(
       ellipse at center,
-      rgba(212, 228, 239, 1) 0%,
-      rgba(134, 174, 204, 1) 100%
+      rgba(228, 233, 237, 1) 0%,
+      rgba(206, 214, 219, 1) 100%
     );
-    filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#d4e4ef', endColorstr='#86aecc',GradientType=1 ); /* IE6-9 fallback on horizontal gradient */
+    filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#e9edf0', endColorstr='#c0c7cc',GradientType=1 ); /* IE6-9 fallback on horizontal gradient */
   }
 
   .weather-input {
--- src/renderer/main.js
+++ src/renderer/main.js
@@ -4,12 +4,28 @@ import axios from 'axios'
 import App from './App'
 import router from './router'
 import store from './store'
+import { LMap, LTileLayer, LMarker, LPopup, LIcon } from "vue2-leaflet";
+
+import "leaflet/dist/leaflet.css";
 
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
 Vue.http = Vue.prototype.$http = axios
 Vue.config.productionTip = false
 axios.defaults.baseURL = 'http://api.openweathermap.org/data/2.5';
+Vue.component("l-map", LMap);
+Vue.component("l-tile-layer", LTileLayer);
+Vue.component("l-marker", LMarker);
+Vue.component("l-popup", LPopup);
+Vue.component("l-icon", LIcon);
+process.env.GOOGLE_API_KEY = 'AIzaSyCFlu2Yl2-UarWqHYi8-uIMDRwWsJ1I3FQ'
 
+import { Icon } from "leaflet";
+delete Icon.Default.prototype._getIconUrl;
+Icon.Default.mergeOptions({
+  iconRetinaUrl: require("leaflet/dist/images/marker-icon-2x.png"),
+  iconUrl: require("leaflet/dist/images/marker-icon.png"),
+  shadowUrl: require("leaflet/dist/images/marker-shadow.png")
+});
 /* eslint-disable no-new */
 new Vue({
   components: { App },
